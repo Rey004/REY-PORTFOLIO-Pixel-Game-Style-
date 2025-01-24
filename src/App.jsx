41366ca0d1487309dff995b6ce18a4c3edeaa948
Revanshu Pusadkar <revanshu444@gmail.com>
2025-01-24 20:56:11 +0530

    initial changes done, project is ready to code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Homepage from './pages/home_page/Homepage.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Homepage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/home_page/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.css'
+
+const Homepage = () => {
+  return (
+    <section className='homepage'>
+      <h1>Hello</h1>
+    </section>
+  )
+}
+
+export default Homepage
