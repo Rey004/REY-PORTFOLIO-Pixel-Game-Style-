8e58ec4acc91b8d7885baf7e4656f13a118e3b6a
Revanshu Pusadkar <revanshu444@gmail.com>
2025-01-26 00:31:00 +0530

    favicon added, main-bg & home-nav components completed, desktop home page completed

---
+++ src/components/home_navbar/Home_nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home_nav.css'
+
+const Home_nav = () => {
+  return (
+    <nav className='home_nav'>
+        <h1 className="logo">REY.</h1>
+        <button className="contact_button">CONTACT ME</button>
+    </nav>
+  )
+}
+
+export default Home_nav
--- src/pages/home_page/Homepage.jsx
+++ src/pages/home_page/Homepage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './homepage.css'
+import Home_nav from '../../components/home_navbar/Home_nav.jsx'
+import Main_bg from '../../components/main_bg/Main_bg.jsx'
 
 const Homepage = () => {
   return (
     <section className='homepage'>
-      <h1>Hello</h1>
+      <Home_nav/>
+      <div className="home_content">
+        <h1 className='name'>I'm <span>REVANSHU</span></h1>
+        <h1 className='profession'>DEVELOPER & DESIGNER</h1>
+        <div className="buttons">
+          <button className='about_button'>About Me</button>
+          <button className='portfolio_button'>Portfolio</button>
+        </div>
+      </div>
+      <video src="assets/character.webm" loop autoPlay muted></video>
+      <Main_bg/>
     </section>
   )
 }
