bb6e07d2688f8fb91e4af86c83e2f7abc7f7698a
Revanshu Pusadkar <revanshu444@gmail.com>
2025-02-08 14:41:49 +0530

    path of the background image of projects changed

--- src/pages/portfolio_page/Project/Code_Vantage.jsx
+++ src/pages/portfolio_page/Project/Code_Vantage.jsx
@@ -6,7 +6,7 @@ const Project1 = () => {
     return (
         <ProjectWrapper>
             <Portfolioproject 
-            backgroundImage={'../../../public/assets/Code_Vantage_bg.webp'}
+            backgroundImage={'/assets/Code_Vantage_bg.webp'}
             project_name={'Code Vantage'}
             project_overview={'Code Vantage is a Web Design Agency which is co-founded by me and I have designed the the UI, 3D elements that are included in the website and developed it with React.This website is interactive, futuristic and visually appealing. '}
             role={'UI/UX Designer, Developer'}
--- src/pages/portfolio_page/Project/Project2.jsx
+++ src/pages/portfolio_page/Project/Project2.jsx
@@ -6,7 +6,7 @@ const Project2 = () => {
     return (
         <ProjectWrapper>
             <Portfolioproject 
-            backgroundImage={'../../../public/assets/Rey-Ventures.webp'}
+            backgroundImage={'/assets/Rey-Ventures.webp'}
             project_name={'Rey Ventures'}
             project_overview={'My purpose behind starting Rey Ventures is to aware people about the things which are relevant and are useful to make an impact on the society so that th world can be a better place . in this channel I’ll be putting all of my learnings and the updates related my entrepreneurial life so that you guys can learn from my Lifestyle. Join me in my journey to escape the Rat Race.'}
             role={'UI/UX Designer, Developer'}
